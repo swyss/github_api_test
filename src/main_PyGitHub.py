bed19597c80405984fdc4f5a5ecf2dd4433a294e
s.wyss <stefan.arnold.wyss@gmail.com>
2024-03-27 23:52:58 +0100

    bug: token is missing

--- src/main_PyGitHub.py
+++ src/main_PyGitHub.py
@@ -1,7 +1,7 @@
 from github import Github
 
 # Github Enterprise with custom hostname
-g = Github(base_url="https://{swyss}/api/v3", auth=auth)
+g = Github(base_url="https://{swyss}/api/v3", login_or_token="your_token") # add token
 
 # Then play with your Github objects:
 for repo in g.get_user().get_repos():
